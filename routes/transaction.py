b10843de12389434d82ddbcbf6398cfaa9f63396
NafizEkbal <nafizekbal.ruet.cse21@gmail.com>
2026-04-18 01:15:57 +0600

    Implement transaction income and expense  history, update and delete functionality

--- routes/transaction.py
+++ routes/transaction.py
@@ -21,4 +21,62 @@ def add_transaction():
         conn.commit()
         return redirect(url_for("dashboard.home"))
 
-    return render_template("add_transaction.html")    
+    return render_template("add_transaction.html")  
+
+
+@transaction.route("/income")
+def income():
+    if "user_id" not in session:
+        return redirect(url_for("auth.login"))
+    
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM `transactions` WHERE transaction_type = 'income' and user_id = %s ORDER BY created_at DESC",(session["user_id"],))
+    income = cursor.fetchall()
+
+    return render_template("income.html",income = income)
+
+
+@transaction.route("/expense")
+def expense():
+    if "user_id" not in session:
+        return redirect(url_for("auth.login"))
+    
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM `transactions` WHERE transaction_type = 'expense' and user_id = %s ORDER BY created_at DESC",(session["user_id"],))
+    expense = cursor.fetchall()
+
+    return render_template("expense.html",expense = expense)
+
+
+@transaction.route("/edit_transaction/<int:id>", methods = ['POST','GET'])
+def edit_transaction(id):
+    if "user_id" not in session:
+        return redirect(url_for("auth.login"))
+
+    action = request.form.get("action")
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM transactions WHERE user_id = %s AND id = %s",(session["user_id"],id))
+    trans = cursor.fetchone()
+
+    if action == "delete":
+        cursor.execute("DELETE FROM transactions WHERE user_id= %s AND id= %s",(session["user_id"],id))
+        conn.commit()
+        return redirect(url_for("dashboard.home"))
+
+    if action == "update":
+        title = request.form.get("title")
+        category = request.form.get("category")
+        transactionType = request.form.get("transactionType")
+        amount = request.form.get("amount")
+        note = request.form.get("note")
+
+        cursor.execute("UPDATE transactions SET title=%s, category=%s, transaction_type=%s, amount=%s, note=%s WHERE user_id= %s AND id= %s",(title, category, transactionType, amount, note, session["user_id"], id))
+        conn.commit()
+        return redirect(url_for("dashboard.home"))
+
+    print(trans)
+    return render_template("edit_transaction.html",transaction = trans)
